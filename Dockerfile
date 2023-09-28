680e396e32e9ba3e45dddf66b145366744892c7e
Michael Logic <ml@michaellogic.com>
2023-09-28 02:55:00 -0500

    add Google Analytics

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:alpine
 
-LABEL version="1.7.16"
+LABEL version="1.7.18"
 LABEL description="DreamMerchant Marketing App"
 LABEL maintainer = ["ml@michaellogic.com"]
 
--- pages/_app.js
+++ pages/_app.js
@@ -62,6 +62,18 @@ export default function App({ Component, pageProps }) {
       </Head>
       {!loaded && <PreLoader />}
       {loaded && <Component {...pageProps} />}
+
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-39572249-2"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-39572249-2');
+      </script>
+
+
     </Fragment>
   );
 }
